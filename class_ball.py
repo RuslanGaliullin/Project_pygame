34b4add293c43c566ea1689de81680b61839a4ae
fatahov <fatahoff.georgy@yandex.ru>
2019-01-29 19:36:14 +0300

    change class

--- class_ball.py
+++ class_ball.py
@@ -26,19 +26,22 @@ def load_image(name, colorkey=None):
 class Ball(pygame.sprite.Sprite):
     image = load_image("ball.png")
 
-    def __init__(self, group, a, v):
+    def __init__(self, group, a, v, x, y):
         # НЕОБХОДИМО вызвать конструктор родительского класса Sprite.
         # Это очень важно !!!
         super().__init__(group)
         self.image = Ball.image
         self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.top = y
+        self.rect.y = y
         self.v = v
         self.a = a
 
     def update(self):
         self.rect.x += 1
         self.rect.y = int(self.rect.x * math.tan(math.radians(self.a)) - (9.8 * self.rect.x ** 2) / (
-                2 * self.v ** 2 * math.cos(math.radians(self.a)) ** 2))
+                2 * self.v ** 2 * math.cos(math.radians(self.a)) ** 2)) + self.top
 
     # Поручим бомбочке получать событие и взрываться (поменяем картинку)
     def get_event(self, event):
